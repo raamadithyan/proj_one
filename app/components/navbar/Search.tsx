654ff613c0ed91b9e0ac9b43b7f496b03ea7b4fa
raamadithyan <chitha.don@gmail.com>
2023-04-14 02:06:56 +0530

    changed search icon to purple

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -14,7 +14,7 @@ function Search() {
         <div className="hidden  text-sm text-red-500 font-semibold px-6  border-x-[1px] border-red-400 md:block">Any Week</div>
         <div className="hidden  text-sm text-red-400 font-semibold pl-3 pr-6  md:block ">Any Week</div>
         <div className="">
-          <div className="p-1 md:p-2 bg-rose-500 rounded-full text-white mr-1">
+          <div className="p-1 md:p-2 bg-purple-500 rounded-full text-white mr-1">
             <BiSearch size={18} />
           </div>
         </div>
